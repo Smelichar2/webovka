3e1f3fc5da095ecfacec295b4b70ea893c4927ed
smelichar1 <dalsiemail7@seznam.cz>
2023-11-22 11:48:06 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import NavigationMenu from "./components/navigation/navigation_menu";
 import Portfolio from "./pages/portfolio";
 import Carousel from "./components/carousels/carousel";
+import Skillset from "./components/skills/skillset";
+import Header from "./components/header/header";
 
 function App() {
   return (
 <div>
-
+  <Header/>,
   <NavigationMenu/>,
   <Portfolio/>,
-  <Carousel/>
+  <Carousel/>,
+  <Skillset/>,
 </div>
 
   );
---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './header.css'; // Importujte příslušný CSS soubor
+
+const Header = () => {
+    return (
+        <div classname="header">
+            <h1>Můj Web</h1>
+        </div>
+    );
+};
+export default Header;
